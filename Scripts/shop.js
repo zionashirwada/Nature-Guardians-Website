fb9defe54a99840c2a3845ff727c6fb3a68e8d17
Ashirwada Manamudali <91813126+zionashirwada@users.noreply.github.com>
2024-03-05 19:39:23 +0530

    sho[

--- Scripts/shop.js
+++ Scripts/shop.js
@@ -1,26 +1,30 @@
-const images = ['Images/shop/can1.svg', 'Images/shop/can2.svg', 'Images/shop/can3.svg'];
-const colors = ['#C9E78A', '#85C1E9', '#F7DC6F'];
+const images = ['Images/shop/t1.png', 'Images/shop/t2.png', 'Images/shop/t3.png'];
+const colors = ['#C9E78A', '#F7DC6F', '#85C1E9'];
+const secondcolors = ['#086b45', '#d17d0e', '#304f73'];
+const titles = ['Monkey print T-shirt', 'Tiger print T-shirt', 'Elephant print T-shirt'];
 let currentIndex = 0;
 
+function updateImageAndColor() {
+    document.getElementById('timage').src = images[currentIndex];
+    document.getElementById('banner_container').style.backgroundColor = colors[currentIndex];
+    document.getElementById('t-title').innerHTML = titles[currentIndex];
+    document.getElementById('t-title').style.color = secondcolors[currentIndex];
+    document.getElementById('t-price').style.backgroundColor = secondcolors[currentIndex];
+    document.getElementById('t-button').style.color = secondcolors[currentIndex];
+    document.getElementById('t-button').style.borderColor = secondcolors[currentIndex];4
+    
+}
 function prevImage() {
     currentIndex = (currentIndex - 1 + images.length) % images.length;
     updateImageAndColor();
-    document.getElementById('banner_img').style.transform = 'translateX(100%)';
-    setTimeout(() => {
-        document.getElementById('banner_img').style.transform = 'translateX(0)';
-    }, 50);
+    document.getElementById('banner_img');
+    
 }
 
 function nextImage() {
     currentIndex = (currentIndex + 1) % images.length;
     updateImageAndColor();
-    document.getElementById('banner_img').style.transform = 'translateX(-100%)';
-    setTimeout(() => {
-        document.getElementById('banner_img').style.transform = 'translateX(0)';
-    }, 50);
+    document.getElementById('banner_img');
+    
 }
 
-function updateImageAndColor() {
-    document.getElementById('canImage').src = images[currentIndex];
-    document.getElementById('banner_container').style.backgroundColor = colors[currentIndex];
-}
