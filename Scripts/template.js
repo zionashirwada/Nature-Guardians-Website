b8b3608493301928cfb632969e2cbff24ea3b852
Ashirwada Manamudali <91813126+zionashirwada@users.noreply.github.com>
2024-03-30 05:45:33 +0530

    .

---
+++ Scripts/template.js
@@ -0,0 +1,17 @@
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+    mybutton.style.display = "flex";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0;
+}
